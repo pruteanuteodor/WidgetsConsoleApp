f15799e65778a5369f3e935373b18e2ad899abf6
pruteanuteodor <pruteanuteodor@yahoo.com>
2020-03-09 07:30:04 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace WidgetsConsoleAppNetCore {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Initialising objects");
+
+			Square s = new Square(10, 20, 100);
+			Rectangle r = new Rectangle(120, 20, 50, 60);
+			Circle c = new Circle(40, 100, 20);
+			Elipse e = new Elipse(150, 250, 10, 20);
+			TextBox t1 = new TextBox(10, 10, 300, 30, "Test", Color.White);
+			TextBox t2 = new TextBox(10, 50, 300, 30);
+			Canvas cList = new Canvas(new List<Widget>() {
+				new Square(10, 20, 100),
+				new Rectangle(10, 20, 30, 40), 
+				new Circle(40, 100, 20),
+				new TextBox(10, 10, 300, 30) 
+			});
+			Console.WriteLine("Initialising complete\n");
+
+			Console.WriteLine("Rendering Widgets");
+			s.Draw();
+			r.Draw();
+			c.Draw();
+			e.Draw();
+			t1.Draw();
+			t2.Draw();
+			Console.WriteLine("Done\n");
+
+			Console.WriteLine("Rendering Canvas");
+			cList.Draw();
+			Console.WriteLine("Done\n");
+
+			Console.WriteLine("Rendering complete");
+		}
+	}
+}
---
+++ Widgets.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace WidgetsConsoleAppNetCore {
+	abstract class Widget {
+		public string Name { get; set; }
+		public int X { get; set; }
+		public int Y { get; set; }
+		public int Size { get; set; }
+		public abstract void Draw();
+	}
+
+	class Square : Widget {
+		public override void Draw() => Console.WriteLine(Name + " (x: " + X + ", y: " + Y + ") Size: " + Size);
+		public Square(int x, int y, int size) {
+			Name = "Square";
+			X = x;
+			Y = y;
+			Size = size;
+		}
+	}
+
+	class Rectangle : Widget {
+		public int Width { get; set; }
+		public int Height { get; set; }
+		public override void Draw() => Console.WriteLine(Name + " (x: " + X + ", y: " + Y + ") Width: " + Width + " Height: " + Height);
+		public Rectangle(int x, int y, int width, int height) {
+			Name = "Rectangle";
+			X = x;
+			Y = y;
+			Width = width;
+			Height = height;
+		}
+	}
+
+	class Circle : Widget {
+		public override void Draw() => Console.WriteLine(Name + " (x: " + X + ", y: " + Y + ") Diameter: " + Size);
+		public Circle(int x, int y, int size) {
+			Name = "Circle";
+			X = x;
+			Y = y;
+			Size = size;
+		}
+	}
+
+	class Elipse : Widget {
+		public int vDiameter { get; set; }
+		public int hDiameter { get; set; }
+		public override void Draw() => Console.WriteLine(Name + " (x: " + X + ", y: " + Y + ") Horisontal diameter: " + hDiameter + " Vertical diameter: " + vDiameter);
+		public Elipse(int x, int y, int hD, int vD) {
+			Name = "Elipse";
+			X = x;
+			Y = y;
+			hDiameter = hD;
+			vDiameter = vD;
+		}
+	}
+
+	class TextBox : Rectangle {
+		public Color BackgroundColor { get; set; }
+		public string Text { get; set; }
+		public override void Draw() {
+			Console.WriteLine(Name + " (x: " + X + ", y: " + Y + ") Width: " + Width + " Height: " + Height + (Text == ""? "" : " Text: " + Text) + " Background color: " + BackgroundColor);
+		}
+		public TextBox(int x, int y, int width, int height, string text, Color bgColor) : base(x, y, width, height) {
+			Name = "TextBox";
+			X = x;
+			Y = y;
+			Width = width;
+			Height = height;
+			Text = text;
+			BackgroundColor = bgColor;
+		}
+		public TextBox(int x, int y, int width, int height) : base(x, y, width, height) {
+			Name = "TextBox";
+			X = x;
+			Y = y;
+			Width = width;
+			Height = height;
+			Text = "";
+			BackgroundColor = Color.Red;
+		}
+	}
+
+	class Canvas {
+		public List<Widget> Widgets { get; set; }
+		public Canvas(List<Widget> widgets) => Widgets = widgets;
+
+		public void Draw() {
+			foreach(Widget w in Widgets) {
+				w.Draw();
+			}
+		}
+	}
+}
